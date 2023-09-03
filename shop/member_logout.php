31b28df837062a0ffc759491e5b13f3b0dc75624
mori_web <minipla0318@gmail.com>
2023-09-03 14:43:02 +0900

    9-2-4 ログアウト機能の実装 完了

---
+++ shop/member_logout.php
@@ -0,0 +1,21 @@
+<?php
+session_start();  // セッションの開始
+$_SESSION = array();  // セッション変数を空にする
+if(isset($_COOKIE[session_name()]) == true) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  ログアウトしました。<br>
+  <br>
+  <a href="shop_list.php">商品一覧へ</a>
+</body>
+</html>
